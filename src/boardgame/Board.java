a6201f6c2d2a468fae1d8a76e2d99693d2354be9
Tales Nunes <talesnunes.siqueira@gmail.com>
2024-01-23 09:43:04 -0300

    Board UI and Firsts tests of position

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -29,4 +29,8 @@ public class Board {
     public Piece piece (int row, int column) {
         return pieces[row][column];
     }
+    public void placePiece(Piece piece, Position position) {
+        pieces[position.getRow()][position.getColumn()] = piece;
+        piece.position = position;
+    }
 }
