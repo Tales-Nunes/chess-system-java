aacb2e35a2475b2d34fd3baecf7d43661519eff8
Tales Nunes <talesnunes.siqueira@gmail.com>
2024-01-24 13:49:19 -0300

    Moving Pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -4,6 +4,7 @@ import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
 
+import java.sql.SQLOutput;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -28,6 +29,10 @@ public class UI {
     public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+    public static void clearScreen() {
+        System.out.println("\033[H\033[2j");
+        System.out.flush();
+    }
     public static ChessPosition readChessPosition(Scanner sc){
         try {
             String s = sc.nextLine();
