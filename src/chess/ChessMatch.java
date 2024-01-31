e4ce451b925a9d2f8da4dfdb30429c755d74b974
Tales Nunes <talesnunes.siqueira@gmail.com>
2024-01-31 15:19:32 -0300

    CheckMate Logic

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -6,6 +6,7 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
+import chess.pieces.Pawn;
 import chess.pieces.Rook;
 public class ChessMatch {
     private int turn;
@@ -81,7 +82,8 @@ public class ChessMatch {
     }
 
     private Piece makeMove(Position source, Position target) {
-        Piece p = board.removePiece(source);
+        ChessPiece p = (ChessPiece)board.removePiece(source);
+        p.increaseMoveCount();
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
 
@@ -94,7 +96,8 @@ public class ChessMatch {
     }
 
     private void undoMove(Position source, Position target, Piece capturedPiece) {
-        Piece p = board.removePiece(target);
+        ChessPiece p = (ChessPiece)board.removePiece(target);
+        p.decreaseMoveCount();
         board.placePiece(p, source);
 
         if (capturedPiece != null) {
@@ -190,5 +193,8 @@ public class ChessMatch {
 
         placeNewPiece('b', 8, new Rook(board, Color.BLACK));
         placeNewPiece('a', 8, new King(board, Color.BLACK));
+
+        placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('h', 6, new Pawn(board, Color.BLACK));
     }
 }
